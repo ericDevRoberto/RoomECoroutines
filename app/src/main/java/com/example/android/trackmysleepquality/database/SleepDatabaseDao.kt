848dede376c335a3bdabf18f9c65dc5926f6497c
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-01-08 14:16:06 -0300

    Created a Room Database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,7 +16,36 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao {
+
+    //Put a item in table
+    @Insert
+    fun insert(night: SleepNight)
+
+    //Update a item in the table
+    @Update
+    fun update(night: SleepNight)
+
+    ///Get aespecific item in the table
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight?
+
+    //Delete all itens in the table
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    //Get the last item registered in the table
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
+    //Get all itens in the table
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+}
--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -16,3 +16,22 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Table format!
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+        @PrimaryKey(autoGenerate = true)
+        var nightId: Long = 0L,
+
+        @ColumnInfo(name = "startTimeMilli")
+        val startTimeMilli: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "endTimeMilli")
+        var endTimeMilli: Long = startTimeMilli,
+
+        @ColumnInfo(name = "sleepQuality")
+        var sleepQuality: Int = -1
+)
